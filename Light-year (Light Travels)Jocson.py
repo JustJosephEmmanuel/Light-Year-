a01b353ee221aa63054d7f6dda40d2ae61b6651a
JustJosephEmmanuel <118593754+JustJosephEmmanuel@users.noreply.github.com>
2022-11-29 08:58:33 +0800

    Add files via upload

---
+++ Light-year (Light Travels)Jocson.py
@@ -0,0 +1,5 @@
+rate = 3 * 108
+seconds = 365 * 24 * 602
+distance = rate * seconds #this is to calculate the value of a light-year
+distance = str (distance)
+print("Light travels "+ distance + " meters in a year")
